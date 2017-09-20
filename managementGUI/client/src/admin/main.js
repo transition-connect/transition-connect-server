292363ebae6a50260ed87d99a8992d785a49ce72
Roger Waldvogel <info@elyoos.org>
2017-09-20 15:14:06 +0200

    Polyfill es6 promises for IE.

--- managementGUI/client/src/admin/main.js
+++ managementGUI/client/src/admin/main.js
@@ -3,9 +3,12 @@ import VeeValidate from 'vee-validate';
 import VueRouter from 'vue-router';
 import svgicon from 'vue-svgicon';
 import moment from 'moment';
+import es6Promise from 'es6-promise';
 import "babel-polyfill";
 import App from './App.vue';
 import routes from './routes';
+
+es6Promise.polyfill();
 
 Vue.use(VeeValidate);
 Vue.use(VueRouter);
--- managementGUI/client/src/public/main.js
+++ managementGUI/client/src/public/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import VeeValidate from 'vee-validate';
 import App from './App.vue';
+import es6Promise from 'es6-promise';
+
+es6Promise.polyfill();
 
 Vue.use(VeeValidate);
 
