f4fcaea683f68d64028d21d75157fcac577ec3b0
Roger Waldvogel <info@elyoos.org>
2017-11-28 10:10:11 +0100

    Handling error when connection or import fails.

--- syncService/src/connectionHandler/importNpEvents.js
+++ syncService/src/connectionHandler/importNpEvents.js
@@ -5,29 +5,37 @@ let importEventToDb = requireSyncLogic('import/event/npEvents');
 let adapter = requireAdapter('networkingPlatform/index');
 let logger = require('server-lib').logging.getLogger(__filename);
 
-let importEvent = async function(npConfig, uid, timestamp) {
-    let event = await adapter.importEvent(npConfig.config.npApiUrl, uid);
-    await importEventToDb.importEvent(uid, timestamp, event.iCal, event.idOrg, npConfig.np.platformId);
+let importEvent = async function (npConfig, uid, timestamp) {
+    try {
+        let event = await adapter.importEvent(npConfig.config.npApiUrl, uid);
+        await importEventToDb.importEvent(uid, timestamp, event.iCal, event.idOrg, npConfig.np.platformId);
+    } catch (error) {
+        logger.error(`Import of event ${uid} failed`);
+    }
 };
 
 let importEvents = async function (npConfig) {
     let events, skip = 0, numberOfLoop = 0;
     const MAX_NUMBER_OF_LOOP = 500;
-    do {
-        numberOfLoop++;
-        events = await adapter.getListEvents(npConfig.config.npApiUrl, skip);
-        if (events && events.events && events.events.length > 0) {
-            let eventsToImport = await difference.getEventsToImport(events.events, npConfig.np.platformId);
-            for(let event of eventsToImport) {
-                await importEvent(npConfig, event.uid, event.timestamp);
+    try {
+        do {
+            numberOfLoop++;
+            events = await adapter.getListEvents(npConfig.config.npApiUrl, skip);
+            if (events && events.events && events.events.length > 0) {
+                let eventsToImport = await difference.getEventsToImport(events.events, npConfig.np.platformId);
+                for (let event of eventsToImport) {
+                    await importEvent(npConfig, event.uid, event.timestamp);
+                }
+                skip = skip + events.events.length;
             }
-            skip = skip + events.events.length;
-        }
-        if (MAX_NUMBER_OF_LOOP <= numberOfLoop) {
-            logger.error(`Max loop ${MAX_NUMBER_OF_LOOP} exceeded for np ${npConfig.np.platformId}`);
-        }
-    } while (events && events.events && events.events.length > 0 &&
-    MAX_NUMBER_OF_LOOP > numberOfLoop);
+            if (MAX_NUMBER_OF_LOOP <= numberOfLoop) {
+                logger.error(`Max loop ${MAX_NUMBER_OF_LOOP} exceeded for np ${npConfig.np.platformId}`);
+            }
+        } while (events && events.events && events.events.length > 0 &&
+        MAX_NUMBER_OF_LOOP > numberOfLoop);
+    } catch (error) {
+        logger.error(`Error when connecting to ${npConfig.config.npApiUrl}`);
+    }
 };
 
 module.exports = {
--- syncService/src/connectionHandler/importOrg.js
+++ syncService/src/connectionHandler/importOrg.js
@@ -5,29 +5,37 @@ let importOrg = requireSyncLogic('import/organization/import');
 let adapter = requireAdapter('networkingPlatform/index');
 let logger = require('server-lib').logging.getLogger(__filename);
 
-let importOrganization = async function(npConfig, id, timestamp) {
-    let organization = await adapter.importOrganisation(npConfig.config.npApiUrl, id);
-    await importOrg.importOrganization(id, timestamp, organization, npConfig.np.platformId);
+let importOrganization = async function (npConfig, id, timestamp) {
+    try {
+        let organization = await adapter.importOrganisation(npConfig.config.npApiUrl, id);
+        await importOrg.importOrganization(id, timestamp, organization, npConfig.np.platformId);
+    } catch (error) {
+        logger.error(`Import of organisation ${id} failed`);
+    }
 };
 
 let importOrganizations = async function (npConfig) {
     let organizations, skip = 0, numberOfLoop = 0;
     const MAX_NUMBER_OF_LOOP = 500;
-    do {
-        numberOfLoop++;
-        organizations = await adapter.getListOrganisations(npConfig.config.npApiUrl, skip);
-        if (organizations && organizations.organizations && organizations.organizations.length > 0) {
-            let organizationsToImport = await difference.getOrgToImport(organizations.organizations, npConfig.np.platformId);
-            for(let org of organizationsToImport) {
-                await importOrganization(npConfig, org.id, org.timestamp);
+    try {
+        do {
+            numberOfLoop++;
+            organizations = await adapter.getListOrganisations(npConfig.config.npApiUrl, skip);
+            if (organizations && organizations.organizations && organizations.organizations.length > 0) {
+                let organizationsToImport = await difference.getOrgToImport(organizations.organizations, npConfig.np.platformId);
+                for (let org of organizationsToImport) {
+                    await importOrganization(npConfig, org.id, org.timestamp);
+                }
+                skip = skip + organizations.organizations.length;
             }
-            skip = skip + organizations.organizations.length;
-        }
-        if (MAX_NUMBER_OF_LOOP <= numberOfLoop) {
-            logger.error(`Max loop ${MAX_NUMBER_OF_LOOP} exceeded for np ${npConfig.np.platformId}`);
-        }
-    } while (organizations && organizations.organizations && organizations.organizations.length > 0 &&
-    MAX_NUMBER_OF_LOOP > numberOfLoop);
+            if (MAX_NUMBER_OF_LOOP <= numberOfLoop) {
+                logger.error(`Max loop ${MAX_NUMBER_OF_LOOP} exceeded for np ${npConfig.np.platformId}`);
+            }
+        } while (organizations && organizations.organizations && organizations.organizations.length > 0 &&
+        MAX_NUMBER_OF_LOOP > numberOfLoop);
+    } catch (error) {
+        logger.error(`Error when connecting to ${npConfig.config.npApiUrl}`);
+    }
 };
 
 module.exports = {
