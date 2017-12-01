1ea63bb86ef977e4aa4c0126b4eabd4f01cf7d7c
Roger Waldvogel <info@elyoos.org>
2017-12-01 18:52:59 +0100

    Show loading an error messages to user.

--- managementGUI/client/src/public/components/login/RequestPassword.vue
+++ managementGUI/client/src/public/components/login/RequestPassword.vue
@@ -7,29 +7,39 @@
                    class="form-control" placeholder="E-Mail">
             <p class="text-danger" v-show="errors.has('email')">Korrekte E-Mail Adresse wird benötigt</p>
         </div>
-        <button v-on:click.prevent="sendPasswordRequest" class="btn btn-primary"
-                :disabled="errors.has('email') || email === '' || !email">Weiter
-        </button>
+        <div id="upload-container">
+            <button v-on:click.prevent="sendPasswordRequest" class="btn btn-primary"
+                    :disabled="errors.has('email') || email === '' || !email || runningRequest">Weiter
+            </button>
+            <loader id="upload-loader" v-show="runningRequest"></loader>
+        </div>
+        <div id="request-error" v-show="loginError">Diese Email Adresse existiert nicht auf Transition Connect</div>
     </form>
 </template>
 
 <script>
     import {HTTP} from './../../../utils/http-common';
+    import Loader from './../../../utils/components/Loader.vue';
 
     export default {
         name: 'request-password',
-        components: {},
+        components: {Loader},
         data: function () {
             return {
-                email: ''
+                email: '', runningRequest: false, loginError: false
             }
         },
         methods: {
             sendPasswordRequest: function () {
+                this.runningRequest = true;
+                this.loginError = false;
                 HTTP.post(`/public/api/login/requestPassword`, {email: this.email}
                 ).then(() => {
+                    this.runningRequest = false;
                     this.$emit('passwordSent', this.email)
                 }).catch(e => {
+                    this.loginError = true;
+                    this.runningRequest = false;
                     console.log(e);
                 })
             }
@@ -46,19 +56,31 @@
         border: solid 1px #ccc;
         padding: 24px;
     }
-
     .email-title {
         margin-top: 0;
         font-size: 24px;
         margin-bottom: 24px;
     }
-
     .email-description {
         font-size: 12px;
         margin-bottom: 12px;
     }
-
     .text-danger {
         margin-top: 6px;
+    }
+    #upload-container {
+        #upload-loader {
+            position: relative;
+            display: inline-block;
+            top: 8px;
+            margin-left: 6px;
+            height: 28px;
+            width: 28px;
+        }
+    }
+    #request-error {
+        margin-top: 6px;
+        font-size: 12px;
+        color: #a94442;
     }
 </style>
--- managementGUI/client/src/public/components/login/VerifyPassword.vue
+++ managementGUI/client/src/public/components/login/VerifyPassword.vue
@@ -6,32 +6,42 @@
             <input v-model="password" v-validate="'required'" type="text" name="password"
                    class="form-control" placeholder="Passwort">
         </div>
-        <button v-on:click.prevent="abort" class="btn btn-default">Abbrechen</button>
-        <button v-on:click.prevent="sendPasswordVerification" class="btn btn-primary"
-                :disabled="errors.has('password') || password === '' || !password">Login
-        </button>
+        <div>
+            <button v-on:click.prevent="abort" class="btn btn-default">Abbrechen</button>
+            <button v-on:click.prevent="sendPasswordVerification" class="btn btn-primary"
+                    :disabled="errors.has('password') || password === '' || !password || runningRequest">Login
+            </button>
+            <loader id="upload-loader" v-show="runningRequest"></loader>
+        </div>
+        <div id="request-error" v-show="loginError">Passwort stimmt nicht oder ist abgelaufen.</div>
     </form>
 </template>
 
 <script>
     import {HTTP} from './../../../utils/http-common';
+    import Loader from './../../../utils/components/Loader.vue';
 
     export default {
         name: 'verify-password',
         props: ['mailAddress'],
-        components: {},
+        components: {Loader},
         data: function () {
             return {
-                password: ''
+                password: '', runningRequest: false, loginError: false
             }
         },
         methods: {
             sendPasswordVerification: function () {
                 console.log(this.mailAddress);
+                this.runningRequest = true;
+                this.loginError = false;
                 HTTP.post(`/public/api/login`, {password: this.password, username: this.mailAddress}
                 ).then(() => {
+                    this.runningRequest = false;
                     window.location.href = "/admin";
                 }).catch(e => {
+                    this.runningRequest = false;
+                    this.loginError = true;
                     console.log(e);
                 })
             },
@@ -65,5 +75,13 @@
 
     .text-danger {
         margin-top: 6px;
+    }
+    #upload-loader {
+        position: relative;
+        display: inline-block;
+        top: 8px;
+        margin-left: 6px;
+        height: 28px;
+        width: 28px;
     }
 </style>
