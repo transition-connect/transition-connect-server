b083d4e62517b19f33ebf53a1e12800c473feaea
Roger Waldvogel <info@elyoos.org>
2017-12-03 13:14:20 +0100

    Compare first events from website with existing events in db. Delete events in db when missing in imported iCal.

--- syncService/src/syncLogic/import/event/websiteEvents.js
+++ syncService/src/syncLogic/import/event/websiteEvents.js
@@ -1,11 +1,12 @@
 'use strict';
 
 let db = require('server-lib').neo4j;
+let _ = require(`lodash`);
 let parser = require('./iCalEventParser');
 let logger = require('server-lib').logging.getLogger(__filename);
 
 let saveEventsToDb = async function (events, organizationId) {
-    return await db.cypher().unwind(`{events} AS event`)
+    await db.cypher().unwind(`{events} AS event`)
         .match(`(org:Organization {organizationId: {organizationId}})`)
         .merge(`(eventDb:Event {uid: event.uid})`)
         .addCommand(` SET eventDb.iCal = event.iCal, eventDb.description = event.description,
@@ -13,12 +14,45 @@ let saveEventsToDb = async function (events, organizationId) {
                       eventDb.startDate = event.startDate, eventDb.endDate = event.endDate`)
         .merge(`(org)-[:WEBSITE_EVENT]->(eventDb)`)
         .end({events: events, organizationId: organizationId}).send();
+    for (let event of events) {
+        logger.info(`For org ${organizationId} -> ${event.summary} (${event.uid} ) event imported from website`);
+    }
+};
+
+let deleteEventFromDb = async function (events, organizationId) {
+    await db.cypher().match(`(org:Organization)-[rel:WEBSITE_EVENT]->(event:Event)`)
+        .where(`event.uid IN {events}`)
+        .delete(`rel, event`)
+        .end({events: _.map(events, 'uid')}).send();
+    for (let event of events) {
+        logger.info(`For org ${organizationId} -> event with uid ${event.uid} deleted`);
+    }
+};
+
+let getChangedOrNewEvents = async function (events, organizationId) {
+    let resp = await db.cypher().unwind(`{events} AS event`)
+        .match(`(org:Organization {organizationId: {organizationId}})
+                -[:WEBSITE_EVENT]->(orgEvent:Event {uid: event.uid})`)
+        .where(`orgEvent.iCal = event.iCal`)
+        .return(`orgEvent.uid AS uid`)
+        .end({events: events, organizationId: organizationId}).send();
+    return _.differenceBy(events, resp, 'uid');
+};
+
+let getEventsToDelete = async function (events, organizationId) {
+    return await db.cypher().match(`(org:Organization {organizationId: {organizationId}})
+                -[:WEBSITE_EVENT]->(orgEvent:Event)`)
+        .where(`NOT orgEvent.uid IN {events}`)
+        .return(`orgEvent.uid AS uid`)
+        .end({events: _.map(events, 'uid'), organizationId: organizationId}).send();
 };
 
 let importEvents = async function (iCal, organizationId) {
     let events = parser.parseEvents(iCal);
-    logger.info(`For org ${organizationId} -> ${events.length} events imported from website`);
-    await saveEventsToDb(events, organizationId);
+    let changedEvents = await getChangedOrNewEvents(events, organizationId);
+    await saveEventsToDb(changedEvents, organizationId);
+    let eventToDelete = await getEventsToDelete(events, organizationId);
+    await deleteEventFromDb(eventToDelete, organizationId);
 };
 
 module.exports = {
--- syncService/test/test/e2e/tests/connectionHandler/import/websiteEventsSpec.js
+++ syncService/test/test/e2e/tests/connectionHandler/import/websiteEventsSpec.js
@@ -4,6 +4,7 @@ let connectionHandler = requireConnectionHandler('connectionHandler');
 let dbDsl = require('server-test-util').dbDSL;
 let db = require('server-test-util').db;
 let requestHandler = require('server-test-util').requestHandler;
+let moment = require('moment');
 let nock = require('nock');
 
 describe('Integration Tests for importing events from a website in iCal format', function () {
@@ -25,46 +26,48 @@ describe('Integration Tests for importing events from a website in iCal format',
         return requestHandler.logout();
     });
 
-    it('Import valid iCal events (all properties)', async function () {
+    it('Import valid iCal events (all properties, end in the future)', async function () {
+
+        let normalStartUtcTimestamp = Math.floor(moment.utc().valueOf() / 1000) - 500,
+            normalEndUtcTimestamp = normalStartUtcTimestamp + 1000,
+            googleStartUtcTimestamp = Math.floor(moment.utc().valueOf() / 1000) + 90000,
+            googleEndUtcTimestamp = googleStartUtcTimestamp + 90000;
 
         nock(`http://www.test.ch`).get('/import/organization')
             .reply(200,
                 `BEGIN:VCALENDAR
-                PRODID:-//Google Inc//Google Calendar 70.9054//EN
-                VERSION:2.0
-                CALSCALE:GREGORIAN
-                METHOD:PUBLISH
-                X-WR-CALNAME:test@gmail.com
-                X-WR-TIMEZONE:Europe/Zurich
-                BEGIN:VEVENT
-                DTSTART:20171027T073000Z
-                DTEND:20171027T083000Z
-                DTSTAMP:20171026T125619Z
-                UID:0c6ckli6gauhsp76ju1vl065dd@google.com
-                CREATED:20171025T150112Z
-                DESCRIPTION:Hat was mit TC zu tun
-                LAST-MODIFIED:20171025T150112Z
-                LOCATION:Irgendwo in Zürich
-                SEQUENCE:0
-                STATUS:CONFIRMED
-                SUMMARY:Event1 Test
-                TRANSP:OPAQUE
-                END:VEVENT
-                BEGIN:VEVENT
-                DTSTART;VALUE=DATE:20171116
-                DTEND;VALUE=DATE:20171117
-                DTSTAMP:20171026T125610Z
-                UID:1c6ckli6gauhsp76ju1vl065dd@google.com
-                CREATED:20171025T1501121Z
-                DESCRIPTION:Hat auch was mit TC zu tun
-                LAST-MODIFIED:20171025T150112Z
-                LOCATION:Irgendwo in Urdorf
-                SEQUENCE:0
-                STATUS:CONFIRMED
-                SUMMARY:Event2 Test
-                TRANSP:OPAQUE
-                END:VEVENT
-                END:VCALENDAR`
+VERSION:2.0
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+X-WR-CALNAME:test@gmail.com
+X-WR-TIMEZONE:Europe/Zurich
+BEGIN:VEVENT
+DTSTART:${moment.utc(normalStartUtcTimestamp * 1000).format('YYYYMMDDTHHmmss')}Z
+DTEND:${moment.utc(normalEndUtcTimestamp * 1000).format('YYYYMMDDTHHmmss')}Z
+UID:0c6ckli6gauhsp76ju1vl065dd@google.com
+CREATED:20171025T150112Z
+DESCRIPTION:Hat was mit TC zu tun
+LAST-MODIFIED:20171025T150112Z
+LOCATION:Irgendwo in Zürich
+SEQUENCE:0
+STATUS:CONFIRMED
+SUMMARY:Event1 Test
+TRANSP:OPAQUE
+END:VEVENT
+BEGIN:VEVENT
+DTSTART;VALUE=DATE:${moment.utc(googleStartUtcTimestamp * 1000).format('YYYYMMDD')}
+DTEND;VALUE=DATE:${moment.utc(googleEndUtcTimestamp * 1000).format('YYYYMMDD')}
+UID:1c6ckli6gauhsp76ju1vl065dd@google.com
+CREATED:20171025T1501121Z
+DESCRIPTION:Hat auch was mit TC zu tun
+LAST-MODIFIED:20171025T150112Z
+LOCATION:Irgendwo in Urdorf
+SEQUENCE:0
+STATUS:CONFIRMED
+SUMMARY:Event2 Test
+TRANSP:OPAQUE
+END:VEVENT
+END:VCALENDAR`
             );
 
         await dbDsl.sendToDb();
@@ -77,42 +80,121 @@ describe('Integration Tests for importing events from a website in iCal format',
         resp[0].event.description.should.equals('Hat was mit TC zu tun');
         resp[0].event.summary.should.equals('Event1 Test');
         resp[0].event.location.should.equals('Irgendwo in Zürich');
-        resp[0].event.startDate.should.equals(1509089400);
-        resp[0].event.endDate.should.equals(1509093000);
+        resp[0].event.startDate.should.equals(normalStartUtcTimestamp);
+        resp[0].event.endDate.should.equals(normalEndUtcTimestamp);
         resp[0].event.iCal.should.equals(`BEGIN:VEVENT
-                DTSTART:20171027T073000Z
-                DTEND:20171027T083000Z
-                DTSTAMP:20171026T125619Z
-                UID:0c6ckli6gauhsp76ju1vl065dd@google.com
-                CREATED:20171025T150112Z
-                DESCRIPTION:Hat was mit TC zu tun
-                LAST-MODIFIED:20171025T150112Z
-                LOCATION:Irgendwo in Zürich
-                SEQUENCE:0
-                STATUS:CONFIRMED
-                SUMMARY:Event1 Test
-                TRANSP:OPAQUE
-                END:VEVENT`);
+DTSTART:${moment.utc(normalStartUtcTimestamp * 1000).format('YYYYMMDDTHHmmss')}Z
+DTEND:${moment.utc(normalEndUtcTimestamp * 1000).format('YYYYMMDDTHHmmss')}Z
+UID:0c6ckli6gauhsp76ju1vl065dd@google.com
+CREATED:20171025T150112Z
+DESCRIPTION:Hat was mit TC zu tun
+LAST-MODIFIED:20171025T150112Z
+LOCATION:Irgendwo in Zürich
+SEQUENCE:0
+STATUS:CONFIRMED
+SUMMARY:Event1 Test
+TRANSP:OPAQUE
+END:VEVENT`);
         resp[1].event.uid.should.equals('1c6ckli6gauhsp76ju1vl065dd@google.com');
         resp[1].event.description.should.equals('Hat auch was mit TC zu tun');
         resp[1].event.summary.should.equals('Event2 Test');
         resp[1].event.location.should.equals('Irgendwo in Urdorf');
-        resp[1].event.startDate.should.equals(1510790400);
-        resp[1].event.endDate.should.equals(1510876800);
+        resp[1].event.startDate.should.equals(
+            moment.utc(moment.utc(googleStartUtcTimestamp * 1000).format('YYYYMMDD')).valueOf() / 1000);
+        resp[1].event.endDate.should.equals(
+            moment.utc(moment.utc(googleEndUtcTimestamp * 1000).format('YYYYMMDD')).valueOf() / 1000);
         resp[1].event.iCal.should.equals(`BEGIN:VEVENT
-                DTSTART;VALUE=DATE:20171116
-                DTEND;VALUE=DATE:20171117
-                DTSTAMP:20171026T125610Z
-                UID:1c6ckli6gauhsp76ju1vl065dd@google.com
-                CREATED:20171025T1501121Z
-                DESCRIPTION:Hat auch was mit TC zu tun
-                LAST-MODIFIED:20171025T150112Z
-                LOCATION:Irgendwo in Urdorf
-                SEQUENCE:0
-                STATUS:CONFIRMED
-                SUMMARY:Event2 Test
-                TRANSP:OPAQUE
-                END:VEVENT`);
+DTSTART;VALUE=DATE:${moment.utc(googleStartUtcTimestamp * 1000).format('YYYYMMDD')}
+DTEND;VALUE=DATE:${moment.utc(googleEndUtcTimestamp * 1000).format('YYYYMMDD')}
+UID:1c6ckli6gauhsp76ju1vl065dd@google.com
+CREATED:20171025T1501121Z
+DESCRIPTION:Hat auch was mit TC zu tun
+LAST-MODIFIED:20171025T150112Z
+LOCATION:Irgendwo in Urdorf
+SEQUENCE:0
+STATUS:CONFIRMED
+SUMMARY:Event2 Test
+TRANSP:OPAQUE
+END:VEVENT`);
 
+    });
+
+    it('Delete directly non existing event and not exported', async function () {
+
+        let normalStartUtcTimestamp = Math.floor(moment.utc().valueOf() / 1000) - 500,
+            normalEndUtcTimestamp = normalStartUtcTimestamp + 1000;
+
+        dbDsl.createWebsiteEvent('0c6ckli6gauhsp76ju1vl065dd@google.com', {organizationId: '1', iCal: `BEGIN:VEVENT
+DTSTART:${moment.utc(normalStartUtcTimestamp * 1000).format('YYYYMMDDTHHmmss')}Z
+DTEND:${moment.utc(normalEndUtcTimestamp * 1000).format('YYYYMMDDTHHmmss')}Z
+UID:0c6ckli6gauhsp76ju1vl065dd@google.com
+CREATED:20171025T150112Z
+DESCRIPTION:Hat was mit TC zu tun
+LAST-MODIFIED:20171025T150112Z
+LOCATION:Irgendwo in Zürich
+SEQUENCE:0
+STATUS:CONFIRMED
+SUMMARY:Event1 Test
+TRANSP:OPAQUE
+END:VEVENT`});
+
+        dbDsl.createWebsiteEvent('1', {organizationId: '1', iCal: `BEGIN:VEVENT
+DTSTART:${moment.utc(normalStartUtcTimestamp * 1000).format('YYYYMMDDTHHmmss')}Z
+DTEND:${moment.utc(normalEndUtcTimestamp * 1000).format('YYYYMMDDTHHmmss')}Z
+UID:1
+CREATED:20171025T150112Z
+DESCRIPTION:Hat was mit TC zu tun2
+LAST-MODIFIED:20171025T150112Z
+LOCATION:Irgendwo in Zürich2
+SEQUENCE:0
+STATUS:CONFIRMED
+SUMMARY:Event1 Test2
+TRANSP:OPAQUE
+END:VEVENT`});
+
+        nock(`http://www.test.ch`).get('/import/organization')
+            .reply(200,
+                `BEGIN:VCALENDAR
+VERSION:2.0
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+X-WR-CALNAME:test@gmail.com
+X-WR-TIMEZONE:Europe/Zurich
+BEGIN:VEVENT
+DTSTART:${moment.utc(normalStartUtcTimestamp * 1000).format('YYYYMMDDTHHmmss')}Z
+DTEND:${moment.utc(normalEndUtcTimestamp * 1000).format('YYYYMMDDTHHmmss')}Z
+UID:0c6ckli6gauhsp76ju1vl065dd@google.com
+CREATED:20171025T150112Z
+DESCRIPTION:Hat was mit TC zu tun
+LAST-MODIFIED:20171025T150112Z
+LOCATION:Irgendwo in Zürich
+SEQUENCE:0
+STATUS:CONFIRMED
+SUMMARY:Event1 Test
+TRANSP:OPAQUE
+END:VEVENT
+END:VCALENDAR`
+            );
+
+        await dbDsl.sendToDb();
+        await connectionHandler.startSync();
+        let resp = await db.cypher().match("(org:Organization {organizationId: '1'})-[:WEBSITE_EVENT]->(event:Event)")
+            .return(`event`).orderBy(`event.uid`).end().send();
+
+        resp.length.should.equals(1);
+        resp[0].event.uid.should.equals('0c6ckli6gauhsp76ju1vl065dd@google.com');
+        resp[0].event.iCal.should.equals(`BEGIN:VEVENT
+DTSTART:${moment.utc(normalStartUtcTimestamp * 1000).format('YYYYMMDDTHHmmss')}Z
+DTEND:${moment.utc(normalEndUtcTimestamp * 1000).format('YYYYMMDDTHHmmss')}Z
+UID:0c6ckli6gauhsp76ju1vl065dd@google.com
+CREATED:20171025T150112Z
+DESCRIPTION:Hat was mit TC zu tun
+LAST-MODIFIED:20171025T150112Z
+LOCATION:Irgendwo in Zürich
+SEQUENCE:0
+STATUS:CONFIRMED
+SUMMARY:Event1 Test
+TRANSP:OPAQUE
+END:VEVENT`);
     });
 });
